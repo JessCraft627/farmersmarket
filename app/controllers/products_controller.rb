656c60af23692589bc9a104b23e6d4538910c43e
Jessicalong627 <jessicalong627@gmail.com>
2018-08-23 18:15:20 -0400

    almost done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,17 +22,32 @@ class ProductsController < ApplicationController
   end
 
   def find_products
-    @food_type = params[:food_type]
-    @products = Product.where(food_type: @food_type)
-    @pics_array = []
-    if @food_type == "dairy"
-      pic_array = dairy
-  end
-end
-
-  dairy = ["dairy/dai_pid_2003301_z.jpg",   "dairy/dai_natyolk_natwhtlg_z.jpg",
+    dairy = ["dairy/dai_pid_2003301_z.jpg",   "dairy/dai_natyolk_natwhtlg_z.jpg",
     "dairy/dai_pid_2003188_z.jpg", "dairy/dai_pid_2002509_z.jpg","dairy/d1.jpg",
     "dairy/dai_orgval_sourcrm_z.jpg"]
+
+    @pics_array = []
+    icon_array = ["vegetables.jpg", "meats.jpg", "dairy.jpg", "fruits.jpg"]
+    @food_type = params[:food_type]
+    @products = Product.where(food_type: @food_type)
+    if @food_type == "dairy"
+    @icon =  icon_array[2]
+    @pics_array = dairy
+
+    elsif @food_type == "meat"
+       @icon = icon_array[1]
+     elsif @food_type == "vegetable"
+       @icon =  icon_array[0]
+    else
+      @icon =  icon_array[3]
+  end
+
+end
+
+
+
+
+
 
 
 end
