5e43830d66798eaa8ba09a96f64e289f3e66d7e4
Jessicalong627 <jessicalong627@gmail.com>
2018-08-23 16:10:20 -0400

    added new files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,5 +24,15 @@ class ProductsController < ApplicationController
   def find_products
     @food_type = params[:food_type]
     @products = Product.where(food_type: @food_type)
+    @pics_array = []
+    if @food_type == "dairy"
+      pic_array = dairy
   end
+end
+
+  dairy = ["dairy/dai_pid_2003301_z.jpg",   "dairy/dai_natyolk_natwhtlg_z.jpg",
+    "dairy/dai_pid_2003188_z.jpg", "dairy/dai_pid_2002509_z.jpg","dairy/d1.jpg",
+    "dairy/dai_orgval_sourcrm_z.jpg"]
+
+
 end
