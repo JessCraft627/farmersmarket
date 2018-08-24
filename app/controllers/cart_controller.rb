f61dafcca4ca6672c0a4bff70dfc80f92fccf0f4
Jessicalong627 <jessicalong627@gmail.com>
2018-08-24 14:19:53 -0400

    added if to cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,19 +2,23 @@ class CartController < ApplicationController
   before_action :get_products_from_cart, only: :show #get items from cart comes from ApplicationController
   before_action :get_total_price, only: :show
 
+
   def update
-
-    params[:quantity].to_i.times do
-      add_product_to_cart(params[:product_id])
-    end
-
-    # byebug
-
-    flash[:notice] = "Successfully added #{params[:name]} to cart"
-    # redirect_to products_path #redirects to refresh cart
-    food_type = Product.find(params[:product_id])[:food_type]
-    redirect_to food_type_path(food_type) #redirects to refresh cart
-  end
+     if params[:quantity].length == 0
+       1.times do
+         add_product_to_cart(params[:product_id])
+         flash[:notice] = "Successfully added 1 order of #{params[:name]} to your cart"
+       end
+     else
+       params[:quantity].to_i.times do
+         add_product_to_cart(params[:product_id])
+         flash[:notice] = "Successfully added #{params[:quantity]} orders of #{params[:name].pluralize} to your cart"
+       end
+     end
+     # redirect_to products_path #redirects to refresh cart
+  food_type = Product.find(params[:product_id])[:food_type]
+  redirect_to food_type_path(food_type) #redirects to refresh cart
+end
 
   def show
     render :show
