99e783903ff45ec95a17185a96694cf7d9909efe
Jessicalong627 <jessicalong627@gmail.com>
2018-11-06 16:45:54 -0500

    changed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
